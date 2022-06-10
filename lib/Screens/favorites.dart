50b189b8d73e2725cee9906ca9219e561cdcf5ff
kiran Raj <kiran11476@gmail.com>
2022-06-10 14:41:32 +0530

    hosted

--- lib/Screens/favorites.dart
+++ lib/Screens/favorites.dart
@@ -31,6 +31,7 @@ class _FavScreenState extends State<FavScreen> {
             ),
             body: FutureBuilder(
               builder: (context, item) {
+                print(item.data);
                 if (item.data == null) {
                   return const Center(
                       child: Text(
@@ -41,70 +42,76 @@ class _FavScreenState extends State<FavScreen> {
 
                 favoriteSongs = item.data as List<FavoritesEntity>?;
 
-                return ListView.builder(
-                    itemCount: favoriteSongs!.length,
-                    itemBuilder: (context, index) {
-                      return ListTile(
-                        leading: QueryArtworkWidget(
-                            id: favoriteSongs![index].id,
-                            type: ArtworkType.AUDIO,
-                            nullArtworkWidget: Padding(
-                              padding: EdgeInsets.only(top: 5.h),
-                            )),
-                        title: Text(
-                          favoriteSongs![index].title,
-                          style: const TextStyle(color: Colors.white),
-                          overflow: TextOverflow.ellipsis,
-                        ),
-                        subtitle: Text(
-                          favoriteSongs![index].album.toString(),
-                          style: const TextStyle(color: Colors.white),
-                          overflow: TextOverflow.ellipsis,
-                        ),
-                        trailing: PopupMenuButton(
-                          shape: RoundedRectangleBorder(
-                            borderRadius: BorderRadius.all(
-                              const Radius.circular(20.0).r,
+                return favoriteSongs!.isEmpty
+                    ? const Center(
+                        child: Text(
+                        'No favourites songs',
+                        style: TextStyle(color: Colors.white),
+                      ))
+                    : ListView.builder(
+                        itemCount: favoriteSongs!.length,
+                        itemBuilder: (context, index) {
+                          return ListTile(
+                            leading: QueryArtworkWidget(
+                                id: favoriteSongs![index].id,
+                                type: ArtworkType.AUDIO,
+                                nullArtworkWidget: Padding(
+                                  padding: EdgeInsets.only(top: 5.h),
+                                )),
+                            title: Text(
+                              favoriteSongs![index].title,
+                              style: const TextStyle(color: Colors.white),
+                              overflow: TextOverflow.ellipsis,
                             ),
-                          ),
-                          color: const Color.fromARGB(255, 233, 235, 237),
-                          elevation: 30.r,
-                          icon: const Icon(
-                            Icons.more_vert_outlined,
-                            color: Color.fromARGB(255, 246, 246, 246),
-                          ), //don't specify icon if you want 3 dot menu
-                          // color: Colors.blue,
-                          itemBuilder: (context) => [
-                            const PopupMenuItem(
-                              value: 0,
-                              child: Text(
-                                "Remove from Favorites",
-                                style: TextStyle(
-                                    color: Color.fromARGB(255, 0, 0, 0)),
+                            subtitle: Text(
+                              favoriteSongs![index].album.toString(),
+                              style: const TextStyle(color: Colors.white),
+                              overflow: TextOverflow.ellipsis,
+                            ),
+                            trailing: PopupMenuButton(
+                              shape: RoundedRectangleBorder(
+                                borderRadius: BorderRadius.all(
+                                  const Radius.circular(20.0).r,
+                                ),
                               ),
+                              color: const Color.fromARGB(255, 233, 235, 237),
+                              elevation: 30.r,
+                              icon: const Icon(
+                                Icons.more_vert_outlined,
+                                color: Color.fromARGB(255, 246, 246, 246),
+                              ), //don't specify icon if you want 3 dot menu
+                              // color: Colors.blue,
+                              itemBuilder: (context) => [
+                                const PopupMenuItem(
+                                  value: 0,
+                                  child: Text(
+                                    "Remove from Favorites",
+                                    style: TextStyle(
+                                        color: Color.fromARGB(255, 0, 0, 0)),
+                                  ),
+                                ),
+                              ],
+                              onSelected: (item) => {
+                                if (item == 0)
+                                  {
+                                    setState(() {
+                                      _audioRoom.deleteFrom(RoomType.FAVORITES,
+                                          favoriteSongs![index].key);
+                                      Fluttertoast.showToast(
+                                          msg: "Deleted from favorites",
+                                          toastLength: Toast.LENGTH_SHORT,
+                                          gravity: ToastGravity.SNACKBAR,
+                                          timeInSecForIosWeb: 1,
+                                          backgroundColor: const Color.fromARGB(
+                                              255, 174, 27, 16),
+                                          textColor: Colors.white,
+                                          fontSize: 16.0);
+                                    })
+                                  }
+                              },
                             ),
-                          ],
-                          onSelected: (item) => {
-                            if (item == 0)
-                              {
-                                setState(() {
-                                  _audioRoom.deleteFrom(RoomType.FAVORITES,
-                                      favoriteSongs![index].key);
-                                  Fluttertoast.showToast(
-                                      msg: "Deleted from favorites",
-                                      toastLength: Toast.LENGTH_SHORT,
-                                      gravity: ToastGravity.SNACKBAR,
-                                      timeInSecForIosWeb: 1,
-                                      backgroundColor: const Color.fromARGB(
-                                          255, 174, 27, 16),
-                                      textColor: Colors.white,
-                                      fontSize: 16.0);
-                                })
-                              }
-                          },
-                        ),
-                      );
-                    });
+                          );
+                        });
               },
               future: _audioroom.queryFavorites(),
             )));
